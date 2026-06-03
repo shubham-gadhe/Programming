291925f295a50cedfc9180e3280a0b9ce8ff2954
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-03 11:42:41 +0530

    Add files via upload

---
+++ C_Programming/AdditionFunctionIO.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int Addition(int No1, int No2)
+{
+    int Ans = 0;
+    Ans = No1 + No2;
+    return Ans;
+}
+
+int main()
+{
+    int Ret = 0, A = 0, B = 0;
+
+    printf
+    
+    Ret = Addition(A, B);
+
+    printf("Addtion is %d\n", Ret);
+
+    return 0;      
+}
