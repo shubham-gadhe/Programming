3557cf629a5ea82b6acf4a8e7ae77a16d11b363b
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-25 20:39:32 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_065.c
@@ -0,0 +1,60 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_Numbers_Divisible_By_11
+//
+//  Description  : This program accepts an array from the user
+//                 and displays all elements which are divisible
+//                 by 11.
+//
+//  Example:
+//      Input  : 85  66  3  55  93  88
+//      Output : 66  55  88
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Display(int Arr[], int iLength)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] % 11 == 0)
+        {
+            printf("%d\t", Arr[iCnt]);
+        }
+    }
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements : \n", iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ", iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p, iSize);
+    
+    free(p);
+
+    return 0;
+}
