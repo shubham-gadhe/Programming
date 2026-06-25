b4ff3837fe291b4ed71b96909b99cd963a2d6fb7
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-25 20:41:12 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_069.c
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Count_Frequency_Of_11
+//
+//  Description  : This program accepts an array from the user
+//                 and counts how many times the number 11
+//                 appears in the array.
+//
+//  Example:
+//      Input  : 85  66  3  66  93  88 
+//      Output : 0
+//
+//      Input  : 85  11  3  15  11  111 
+//      Output : 2
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Frequency(int Arr[], int iLength)
+{
+    int iCnt = 0, iCount = 0;
+
+    for(iCnt = 1; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] ==  11)
+        {
+            iCount++;
+        }
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements \n", iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ", iCnt + 1);
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Frequency(p, iSize);
+
+    printf("%d", iRet);
+
+    free(p);
+
+    return 0;
+}
