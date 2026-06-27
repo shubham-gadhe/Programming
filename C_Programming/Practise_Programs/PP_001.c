9110e5e9c94f62c66dce4ab53f362bb2f4065379
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-27 23:31:21 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_001.c
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_String
+//
+//  Description  : This program displays the String
+//                 "Jay Ganesh..." on the screen.
+//
+//  Example:
+//      Output : Jay Ganesh...
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh...\n");
+    
+    return 0;
+}
