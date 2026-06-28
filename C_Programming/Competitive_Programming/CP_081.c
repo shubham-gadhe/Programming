5c14a6c05be8ee5b60ab092401cda9dbec47b72b
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-28 20:49:24 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_081.c
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_Alphabet_Pattern
+//
+//  Description  : This program accepts a number from the user
+//                 and displays alphabets to the given count.
+//
+//  Example:
+//      Input  : 5
+//      Output : A  B  C  D  E
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    char ch = '\0';
+
+    for(iCnt = 1, ch = 'A'; iCnt <= iNo; iCnt++, ch++)
+    {
+        printf("%c\t", ch);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of elements : \n");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
