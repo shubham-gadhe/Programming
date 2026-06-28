e98068bb19df0c7c2c700860b9c7186f07bb110f
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-28 22:55:22 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_096.c
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_Repeating_Sequential_Number_Pattern
+//
+//  Description  : This program accepts the number of rows
+//                 and columns from the user and displays
+//                 sequential numbers from 1 to 9 repeatedly
+//                 in row-wise order.
+//
+//  Example:
+//      Input  : 3 4
+//      Output : 1  2  3  4
+//               5  6  7  8
+//               9  1  2  3
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, iCount = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            iCount++;
+            if(iCount > 9)
+            {
+                iCount = 1;
+            }
+            printf("%d\t", iCount);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows and Columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
