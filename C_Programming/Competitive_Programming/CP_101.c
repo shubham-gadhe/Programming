f1515d2348967ed2b72d5199b03eb32321f19f49
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-30 22:16:59 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_101.c
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_Star_Hash_Triangle_Pattern
+//
+//  Description  : This program accepts the number of rows
+//                 and columns from the user and displays
+//                 a triangle pattern using '*' and '#'.
+///
+//  Example:
+//      Input  : 4 4
+//      Output : *  #  #  #
+//               *  *  #  #
+//               *  *  *  #
+//               *  *  *  *
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0; 
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i >= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows and Columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
