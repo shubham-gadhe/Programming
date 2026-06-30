8f54c9ba66ff693e2484f626033da23088955a5a
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-30 22:17:22 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_102.c
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_Inverted_Star_Hash_Triangle_Pattern
+//
+//  Description  : This program accepts the number of rows
+//                 and columns from the user and displays
+//                 an inverted triangle pattern using
+//                 '*' and '#'.
+//
+//  Example:
+//      Input  : 4 4
+//      Output : *  *  *  *
+//               *  *  *  #
+//               *  *  #  #
+//               *  #  #  #
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0; 
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if(i <= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }            
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows and Columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
