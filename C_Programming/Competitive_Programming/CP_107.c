675b873e805773f569e20a20ff6ad511a7cc3ee9
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-06-30 22:19:11 +0530

    Add files via upload

---
+++ C_Programming/Competitive_Programming/CP_107.c
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_Inverted_Left_Triangle_Star_Pattern
+//
+//  Description  : This program accepts the number of rows
+//                 and columns from the user and displays
+//                 an inverted left triangle using '*'.
+////
+//  Example:
+//      Input  : 4 4
+//      Output : *  *  *  *
+//               *  *  *
+//               *  *
+//               *
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j >= i)
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows and Columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
