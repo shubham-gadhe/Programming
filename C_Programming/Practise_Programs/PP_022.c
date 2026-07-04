dfda359ca2fb12bc29b089a7cea341b5a2ba7755
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-04 20:12:04 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_022.c
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : Display_String
+//
+//  Description  : This program displays the message
+//                 "Jay Ganesh..." four times on the
+//                 screen using sequential statements.
+//
+//  Example:
+//      Output : Jay Ganesh...
+//               Jay Ganesh...
+//               Jay Ganesh...
+//               Jay Ganesh...
+//
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+//
+//  Base Version: Displays the message using simple
+//                    sequential print statements.
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int main()
+{
+    // Sequence
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n"); 
+    
+    return 0;
+}
