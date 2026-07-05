4523a39ce08575b5eb64398346cbfe2e6a8d0aaf
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-05 20:06:24 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_003.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : PP_003
+//
+//  Description  : This program displays the numbers
+//                 from 1 to 10 using a for loop.
+//
+//  Example:
+//      Output : 1
+//               2
+//               ..
+//               10
+//
+//////////////////////////////////////////////////////////////////
+
+class PP_003
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 10; iCnt++)
+        {
+            System.out.println(iCnt);
+        }    
+    }
+}
