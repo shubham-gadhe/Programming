a002f2954a07d5115b5b4d565a46abff103039a0
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-05 20:10:42 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_010.java
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : PP_010
+//
+//  Description  : This program accepts a number from the user
+//                 and checks whether it is divisible by both
+//                 3 and 5 using a separate utility class with
+//                 a static method.
+//
+//  Example:
+//      Input  : 15
+//      Output : Number is divisible by 3 and 5
+//
+//////////////////////////////////////////////////////////////////
+
+// TYPE 3
+import java.util.Scanner;
+
+class NumberX
+{
+    public static boolean CheckDivisible(int iNo)
+    {
+        if((iNo % 3 == 0) && (iNo % 5 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }           
+    }
+}
+
+class PP_010
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+        boolean bRet = false;
+
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt(); 
+        
+        bRet = NumberX.CheckDivisible(iValue);   
+        
+        if(bRet == true)
+        {
+            System.out.println("Number is divisible by 3 and 5");
+        }
+        else
+        {
+            System.out.println("Number is not divisible by 3 and 5");
+        }
+    }
+}
