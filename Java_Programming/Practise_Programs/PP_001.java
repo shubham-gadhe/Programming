07766f49d5e4b34050e6c99c26f261ac1f2e6d79
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-05 20:05:43 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_001.java
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name    : PP_001
+//
+//  Description  : This program displays the message
+//                 "Jay Ganesh..." on the screen.
+//
+//  Example:
+//      Output : Jay Ganesh...
+//
+//////////////////////////////////////////////////////////////////
+
+class PP_001
+{
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
