58b465fbda38b253349e6c15aa9c5127889ed39d
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-07 19:19:47 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_037.c
@@ -0,0 +1,65 @@
+/*
+==============================================================================
+ Program     : Display Numbers from 1 to N-1
+
+ Description : Reads an integer from the user and displays all numbers from
+               1 up to (but not including) the specified value.
+
+ Author      : Shubham Gadhe
+==============================================================================
+*/
+
+#include<stdio.h>
+
+/*
+==============================================================================
+ Function    : Display
+
+ Description : Displays all numbers from 1 up to (but not including) the
+               specified value.
+
+ Parameters  : iNo - Upper limit of the sequence (exclusive).
+
+ Returns     : None
+
+ Complexity  : Time  : O(n)
+               Space : O(1)
+==============================================================================
+*/
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    // Display numbers from 1 to (iNo - 1).
+    for(iCnt = 1; iCnt < iNo; iCnt = iCnt += 1)
+    {
+        printf("%d\t", iCnt);
+    }
+    
+    printf("\n");
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+/*
+==============================================================================
+ Sample Input
+------------------------------------------------------------------------------
+ 5
+
+ Sample Output
+------------------------------------------------------------------------------
+ 1    2    3    4
+==============================================================================
+*/
