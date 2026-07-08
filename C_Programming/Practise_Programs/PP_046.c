971234ab50a6378c808bb80ef7d89ec76cb962ba
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-08 23:06:33 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_046.c
@@ -0,0 +1,37 @@
+/*
+==============================================================================
+ Program     : Display "Jay Ganesh..." Five Times
+
+ Description : Displays the message "Jay Ganesh..." five times using a
+               for loop.
+
+ Author      : Shubham Gadhe
+==============================================================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    // Display the message five times.
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+    
+    return 0;
+}
+
+/*
+==============================================================================
+ Sample Output
+------------------------------------------------------------------------------
+ Jay Ganesh...
+ Jay Ganesh...
+ Jay Ganesh...
+ Jay Ganesh...
+ Jay Ganesh...
+==============================================================================
+*/
