324722d9cb5e5a631dc2c74a5c5a5095792556a8
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-08 23:06:55 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_047.c
@@ -0,0 +1,39 @@
+/*
+==============================================================================
+ Program     : Display a String Five Times Using while Loop
+
+ Description : Displays a string five times using a while loop.
+
+ Author      : Shubham Gadhe
+==============================================================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    // Display the string five times.
+    while(iCnt <= 5)
+    {
+        printf("Jay Ganesh...\n");
+        iCnt++;
+    }
+    
+    return 0;
+}
+
+/*
+==============================================================================
+ Sample Output
+------------------------------------------------------------------------------
+ Jay Ganesh...
+ Jay Ganesh...
+ Jay Ganesh...
+ Jay Ganesh...
+ Jay Ganesh...
+==============================================================================
+*/
