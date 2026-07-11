c4ac38bec43ea83c56878d4a450dc8b7ff23c85d
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-11 20:32:37 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_053.c
@@ -0,0 +1,66 @@
+/*
+==============================================================================
+ Program     : Display Digits of a Number
+
+ Description : Reads an integer from the user and displays each digit of the
+               specified number starting from the least significant digit.
+
+ Author      : Shubham Gadhe
+==============================================================================
+*/
+
+#include<stdio.h>
+
+/*
+==============================================================================
+ Function    : DisplayDigits
+
+ Description : Displays each digit of the specified number from right to left.
+
+ Parameters  : iNo - Number whose digits are to be displayed.
+
+ Returns     : None
+
+ Complexity  : Time  : O(d)
+               Space : O(1)
+
+ Note        : d represents the number of digits in the given number.
+==============================================================================
+*/
+void DisplayDigits(int iNo)
+{
+    int iDigit = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        printf("%d\n", iDigit);
+        iNo = iNo / 10;
+    }  
+}
+
+int main()
+{
+    int iValue = 0; 
+    
+    printf("Enter Number : \n");
+    scanf("%d", &iValue);
+
+    DisplayDigits(iValue);
+    
+    return 0;
+}
+
+/*
+==============================================================================
+ Sample Input
+------------------------------------------------------------------------------
+ 751
+
+ Sample Output
+------------------------------------------------------------------------------
+ 1
+ 5
+ 7
+==============================================================================
+*/
