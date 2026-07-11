b7eef6804be8734df460525040cd65722d56dbcf
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-07-11 23:06:49 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_059.c
@@ -0,0 +1,38 @@
+/*
+==============================================================================
+ Program     : Display Array Elements
+
+ Description : Demonstrates how to access and display individual elements of
+               an integer array using their index positions.
+
+ Author      : Shubham Gadhe
+==============================================================================
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int Arr[5] = {10,20,30,40,50};
+
+    // Display each element of the array.
+    printf("%d\n", Arr[0]);  
+    printf("%d\n", Arr[1]);
+    printf("%d\n", Arr[2]);
+    printf("%d\n", Arr[3]);
+    printf("%d\n", Arr[4]);
+
+    return 0;   
+}
+
+/*
+==============================================================================
+ Sample Output
+------------------------------------------------------------------------------
+ 10
+ 20
+ 30
+ 40
+ 50
+==============================================================================
+*/
