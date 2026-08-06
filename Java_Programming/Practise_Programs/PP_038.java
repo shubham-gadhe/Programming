9a5e2fb326fee421d0f978a0be30f540352b27bf
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-06 23:33:41 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_038.java
@@ -0,0 +1,65 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_038
+//
+//  Description       : This program accepts a number from the user
+//                      and displays '*' symbol that many times.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 06/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class PP_038
+{
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Function Name    : Display()
+    //
+    //  Description      : It is used to display '*' symbol
+    //                     the specified number of times.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 06/08/2026
+    //
+    //////////////////////////////////////////////////////////////////
+
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.print("*\t");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the number of elements : ");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      5
+//
+//  Sample Output :
+//      *    *    *    *    *
+//
+//////////////////////////////////////////////////////////////////
