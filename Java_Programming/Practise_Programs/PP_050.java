05cd18ca8fbf834cb0a8c666d5083950baeddfe9
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-06 23:45:13 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_050.java
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_050
+//
+//  Description       : This program accepts a number from the user
+//                      and displays uppercase alphabets starting
+//                      from 'A' for the specified count.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 06/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class PP_050
+{
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Function Name    : Display()
+    //
+    //  Description      : It is used to display uppercase
+    //                     alphabets starting from 'A'
+    //                     for the specified number of times.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 06/08/2026
+    //
+    //////////////////////////////////////////////////////////////////
+
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+        char ch = '\0';
+
+        for(iCnt = 1, ch = 'A'; iCnt <= iNo; iCnt++, ch++)
+        {
+            System.out.print(ch + "\t");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the number of elements : ");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      5
+//
+//  Sample Output :
+//      A    B    C    D    E
+//
+//////////////////////////////////////////////////////////////////
