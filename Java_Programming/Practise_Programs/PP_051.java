4964aa9ec555b8e20bf1fd436e2bf3199467aba7
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-07 20:50:52 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_051.java
@@ -0,0 +1,77 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_051
+//
+//  Description       : This program accepts a number from the user
+//                      and displays uppercase alphabets at odd
+//                      positions while displaying '*' at even
+//                      positions.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 07/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class PP_051
+{
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Function Name    : Display()
+    //
+    //  Description      : It is used to display uppercase
+    //                     alphabets at odd positions and
+    //                     '*' at even positions.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 07/08/2026
+    //
+    //////////////////////////////////////////////////////////////////
+
+    public static void Display(int iNo)
+    {
+        int iCnt = 0;
+        char ch = '\0';
+
+        for(iCnt = 1, ch = 'A'; iCnt <= iNo; iCnt++)
+        {
+            if(iCnt % 2 == 0)
+            {
+                System.out.print("*\t");
+            }
+            else
+            {
+                System.out.print(ch + " \t");
+                ch++;
+            }
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the number of elements : ");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      5
+//
+//  Sample Output :
+//      A    *    B    *    C
+//
+//////////////////////////////////////////////////////////////////
