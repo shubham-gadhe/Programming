0fc0a6d1a5ef7bc2ac4079f544dee29cb08ec0d2
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-07 20:56:15 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_064.java
@@ -0,0 +1,87 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_064
+//
+//  Description       : This program accepts the number of rows
+//                      and columns from the user and displays
+//                      a rectangular pattern of '$' symbols.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 07/08/2026
+//
+//  Time Complexity   : O(r × c)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class Pattern
+{
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Function Name    : Display()
+    //
+    //  Description      : It is used to display a rectangular
+    //                     pattern consisting of '$' symbols
+    //                     according to the specified number
+    //                     of rows and columns.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 07/08/2026
+    //
+    //////////////////////////////////////////////////////////////////
+
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print("$\t");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class PP_064
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter the number of Rows :
+//      4
+//
+//      Enter the number of Column :
+//      4
+//
+//  Sample Output :
+//
+//      $    $    $    $
+//      $    $    $    $
+//      $    $    $    $
+//      $    $    $    $
+//
+//////////////////////////////////////////////////////////////////
