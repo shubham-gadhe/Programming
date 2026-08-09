c767abbc1a679c5161f99436a9e753b27f0e98b7
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-09 23:28:15 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_080.java
@@ -0,0 +1,96 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_080
+//
+//  Description       : This program accepts the number of rows
+//                      and columns from the user and displays
+//                      '%' in the first and last columns, while
+//                      displaying '@' in all the remaining columns.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 09/08/2026
+//
+//  Time Complexity   : O(r × c)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class Pattern
+{
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Function Name    : Display()
+    //
+    //  Description      : It is used to display '%' in the
+    //                     first and last columns, and '@'
+    //                     in all the intermediate columns.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 09/08/2026
+    //
+    //////////////////////////////////////////////////////////////////
+
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(j == 1 || j == iCol)
+                {
+                    System.out.print("%\t");
+                }
+                else
+                {
+                    System.out.print("@\t");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class PP_080
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter the number of Rows :
+//      6
+//
+//      Enter the number of Column :
+//      6
+//
+//  Sample Output :
+//
+//      %    @    @    @    @    %
+//      %    @    @    @    @    %
+//      %    @    @    @    @    %
+//      %    @    @    @    @    %
+//      %    @    @    @    @    %
+//      %    @    @    @    @    %
+//
+//////////////////////////////////////////////////////////////////
