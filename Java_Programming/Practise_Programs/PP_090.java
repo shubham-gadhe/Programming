9944930f8d4cea3ad47f0d39039c2306f2a8f5b2
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-10 23:03:58 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_090.java
@@ -0,0 +1,94 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_090
+//
+//  Description       : This program accepts the number of rows
+//                      and columns from the user and displays
+//                      '*' in a lower triangular pattern.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 10/08/2026
+//
+//  Time Complexity   : O(n² / 2)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class Pattern
+{
+//////////////////////////////////////////////////////////////////
+//
+//  Function Name    : Display()
+//
+//  Description      : It is used to display '*' in a lower
+//                     triangular pattern where the number
+//                     of '*' increases in every row.
+//
+//  Author           : Shubham Somanath Gadhe
+//  Date             : 10/08/2026
+//
+//////////////////////////////////////////////////////////////////
+
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        // Filter for Diagonal Pattern
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid Parameter");
+            System.out.println("Number of rows and columns should be same.");
+            return;
+        }
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= i; j++)
+            {
+                System.out.print("*\t");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class PP_090
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      Enter the number of Rows :
+//      4
+//
+//      Enter the number of Column :
+//      4
+//
+//  Sample Output :
+//
+//      *
+//      *    *
+//      *    *    *
+//      *    *    *    *
+//
+//////////////////////////////////////////////////////////////////
