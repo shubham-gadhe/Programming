8d557e90c0335ce2a3b31dfb63a2b74b97bee3b3
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-12 21:00:36 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_098.java
@@ -0,0 +1,49 @@
+
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_098
+//
+//  Description       : This program displays all elements of
+//                      an array using a for loop.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 12/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(n)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class PP_098
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+
+        int iCnt = 0;
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      No input required.
+//
+//  Sample Output :
+//
+//      10
+//      20
+//      30
+//      40
+//      50
+//
+//////////////////////////////////////////////////////////////////
