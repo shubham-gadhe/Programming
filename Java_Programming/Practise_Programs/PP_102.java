e46c3462926d8ee52434d0ce691b3ba7e1a9d2b1
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-12 21:02:23 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_102.java
@@ -0,0 +1,85 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_102
+//
+//  Description       : This program accepts the number of elements
+//                      and elements of an array from the user and
+//                      displays all elements of the array. It also
+//                      makes the array eligible for garbage collection.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 12/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(n)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class PP_102
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iCnt = 0;
+
+        System.out.println("Enter the number of elements: ");
+        int iLength = sobj.nextInt();
+
+        int Brr[] = new int[iLength];
+
+        System.out.println("Enter the elements: ");
+
+        for(iCnt = 0; iCnt < Brr.length; iCnt++)
+        {
+            Brr[iCnt] = sobj.nextInt();
+        }
+
+        System.out.println("Elements of the Array are: ");
+
+        for(iCnt = 0; iCnt < Brr.length; iCnt++)
+        {
+            System.out.println(Brr[iCnt]);
+        }
+
+        Brr = null;
+
+        System.gc();
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      Enter the number of elements:
+//      5
+//
+//      Enter the elements:
+//      10
+//      20
+//      30
+//      40
+//      50
+//
+//  Sample Output :
+//
+//      Enter the number of elements:
+//      5
+//
+//      Enter the elements:
+//      10
+//      20
+//      30
+//      40
+//      50
+//
+//      Elements of the Array are:
+//      10
+//      20
+//      30
+//      40
+//      50
+//
+//////////////////////////////////////////////////////////////////
