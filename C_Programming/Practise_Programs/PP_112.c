1ff8843b5c925f6a0643f891ceb946f3a0954a03
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-14 23:06:57 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_112.c
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_112
+//
+//  Description       : This program accepts a string from the user
+//                      and displays the first three characters of
+//                      the string using a separate function.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 14/08/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(n)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////
+//
+//  Function Name     : Display()
+//
+//  Description       : It is used to display the first three
+//                      characters of the given string.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 14/08/2026
+//
+//////////////////////////////////////////////////////////////////
+
+void Display(char str[])
+{
+    printf("%c\n", str[0]);
+    printf("%c\n", str[1]);
+    printf("%c\n", str[2]);
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s", Arr);  
+    
+    Display(Arr);
+    
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      Ganesh
+//
+//  Sample Output :
+//
+//      Enter string :
+//      G
+//      a
+//      n
+//
+//////////////////////////////////////////////////////////////////
