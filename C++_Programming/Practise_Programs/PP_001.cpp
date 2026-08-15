2b655fd3fb15b824884bc97f5071e5ae406dfca1
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-15 21:53:28 +0530

    Add files via upload

---
+++ C++_Programming/Practise_Programs/PP_001.cpp
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_001
+//
+//  Description       : This program displays a simple message on
+//                      the screen using cout.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 15/08/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh...";
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      No input required.
+//
+//  Sample Output :
+//
+//      Jay Ganesh...
+//
+//////////////////////////////////////////////////////////////////
