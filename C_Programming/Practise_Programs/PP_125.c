f3153e09f13704ef1360b8b6d5e72a4cc2a2d13f
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-15 22:04:12 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_125.c
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_125
+//
+//  Description       : This program counts the number of spaces
+//                      present in a given string.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 15/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(n)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int CountSpace(const char * str)
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == ' ') 
+        {
+            iCount++;
+        }
+        str++;
+    }
+    return iCount;
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s", Arr);  
+    
+    iRet = CountSpace(Arr);
+
+    printf("Frequency is : %d\n", iRet);
+    
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      Jay Ganesh
+//
+//  Sample Output :
+//
+//      Enter string :
+//      Frequency is : 1
+//
+//////////////////////////////////////////////////////////////////
