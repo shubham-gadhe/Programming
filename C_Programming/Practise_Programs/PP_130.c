167f89204b7f53d5cdea1b6e0e03f4d31ff860cb
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-16 23:17:41 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_130.c
@@ -0,0 +1,77 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_130
+//
+//  Description       : This program displays a string in reverse
+//                      order using character pointers.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 16/08/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(n)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////
+//
+//  Function Name     : ReverseDisplay()
+//
+//  Description       : It is used to display the given string in
+//                      reverse order using character pointers.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 16/08/2026
+//
+//////////////////////////////////////////////////////////////////
+
+void ReverseDisplay(char *str)
+{
+    char *start  = NULL;
+    
+    start = str;
+
+    while(*str != '\0')
+    {
+        str++;
+    }
+
+    str--;
+    
+    while(start <= str)
+    {
+        printf("%c\n",*str);
+        str--;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter String : \n");
+    scanf("%[^'\n]s", Arr);
+
+    ReverseDisplay(Arr);
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      Ganesh
+//
+//  Sample Output :
+//
+//      Enter String :
+//      h
+//      s
+//      e
+//      n
+//      a
+//      G
+//
+//////////////////////////////////////////////////////////////////
