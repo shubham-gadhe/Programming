5adf9276e392096067ee6845e3d1b72d06beed38
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-17 21:57:46 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_139.c
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_139
+//
+//  Description       : This program demonstrates the display of an
+//                      integer value in Decimal, Octal and Hexadecimal
+//                      number systems using format specifiers.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 17/08/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int main()
+{
+    int No = 97;
+
+    printf("Decimal : %d\n", No);
+    printf("Octal : %o\n", No);
+    printf("HexaDecimal : %x\n", No);
+    
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      No input required.
+//
+//  Sample Output :
+//
+//      Decimal : 97
+//      Octal : 141
+//      HexaDecimal : 61
+//
+//////////////////////////////////////////////////////////////////
