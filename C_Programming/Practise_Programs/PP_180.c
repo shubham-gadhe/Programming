05d7ed75758ccacab849cd8be82899ff45b7a608
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-23 23:41:23 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_180.c
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_180
+//
+//  Description       : This program toggles the 3rd and 7th bits of
+//                      a given unsigned integer using the bitwise
+//                      XOR operator and an appropriate bit mask.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 23/08/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+// Position 3rd and 7th
+int main()
+{
+    UINT iMask = 0x00000044;
+    UINT iNo = 0;
+    UINT iResult = 0;
+
+    printf("Enter Number : \n");
+    scanf("%u", &iNo);
+
+    iResult = iNo ^ iMask;
+
+    printf("Updated No : %u\n", iResult);
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      10
+//
+//  Sample Output :
+//
+//      Enter Number :
+//      Updated No : 78
+//
+//////////////////////////////////////////////////////////////////
