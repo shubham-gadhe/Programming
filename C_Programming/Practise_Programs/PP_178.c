48ca935c8a2349d47ffe922c1124408215d15043
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-23 23:40:29 +0530

    Add files via upload

---
+++ C_Programming/Practise_Programs/PP_178.c
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_178
+//
+//  Description       : This program demonstrates the bitwise NOT
+//                      operator by complementing all bits of an
+//                      unsigned integer.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 23/08/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iMask = 0xFFFFFFFF;
+
+    printf("Before : %X\n", iMask);
+
+    iMask = ~iMask;
+
+    printf("After : %X\n", iMask);
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      No input required.
+//
+//  Sample Output :
+//
+//      Before : FFFFFFFF
+//      After : 0
+//
+//////////////////////////////////////////////////////////////////
