f66382106e5e4277092b72c89605ffe7a8e0dbf5
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-08-29 23:07:05 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_129.java
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_129
+//
+//  Description       : This program demonstrates the ASCII value
+//                      of a character and performs arithmetic
+//                      operation on the character value.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 29/08/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+class PP_129
+{
+    public static void main(String A[])
+    {
+        char ch1 = 'A';
+
+        System.out.println(ch1);
+
+        System.out.println(ch1 + 32);       // 97
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//      No input required.
+//
+//  Sample Output :
+//
+//      A
+//      97
+//
+//////////////////////////////////////////////////////////////////
