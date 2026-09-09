4a947cc4aad89c6bdd58ea81b6c8f3cd5d5e5a04
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-09 22:14:41 +0530

    Add files via upload

---
+++ Java_Programming/Practise_Programs/PP_199.java
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_199
+//
+//  Description       : Demonstrates the use of the throws keyword
+//                      to declare an exception that may occur.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 09/09/2026
+//
+//  Time Complexity   : O(1)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
+
+import java.io.*;
+
+class PP_199
+{
+    public static void main(String A[]) throws Exception
+    {
+        FileReader fobj = new FileReader("Demo.txt");
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input :
+//
+//  Sample Output :  
+//
+//////////////////////////////////////////////////////////////////
