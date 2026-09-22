234c242098e35d14dccde7af7c726deeb165d70a
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-22 22:25:26 +0530

    Update PP_031.java

--- Java_Programming/Practise_Programs/PP_031.java
+++ Java_Programming/Practise_Programs/PP_031.java
@@ -1,56 +1,53 @@
-/*
-==============================================================================
- Program     : Count Odd Digits in a Number
-
- Description : Reads an integer from the user and counts the number of odd
-               digits present in the specified number.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_031
+//
+//  Description       : This program accepts a number from the user
+//                      and counts the number of odd digits present
+//                      in the given number.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 10/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 import java.util.Scanner;
 
-/*
-==============================================================================
- Class       : DigitX
-
- Description : Provides utility methods for performing operations on digits.
-==============================================================================
-*/
 class DigitX
 {
-     /*
-    ==============================================================================
-     Function    : CountOddDigits
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Method Name      : CountOddDigits()
+    //
+    //  Description      : It is used to count the number of odd
+    //                     digits present in the given number by
+    //                     extracting each digit and checking
+    //                     whether it is odd.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 10/07/2026
+    //
+    //////////////////////////////////////////////////////////////////
 
-     Description : Counts the number of odd digits present in the specified
-                   number.
-
-     Parameters  : iNo - Number whose digits are to be examined.
-
-     Returns     : Number of odd digits present in the specified number.
-
-     Complexity  : Time  : O(d)
-                   Space : O(1)
-
-     Note        : d represents the number of digits in the given number.
-    ==============================================================================
-    */    
     public int CountOddDigits(int iNo)
     {
         int iDigit = 0;
         int iCount = 0;
 
-         while(iNo != 0)
+        while(iNo != 0)
         {
             iDigit = iNo % 10;
+
             if(iDigit % 2 != 0)
             {
                 iCount++;
-            }  
-            iNo = iNo / 10;      
-        }  
+            }
+
+            iNo = iNo / 10;
+        }
 
         return iCount;
     }
@@ -65,30 +62,23 @@ class PP_031
 
         int iValue = 0;
         int iRet = 0;
-        
+
         System.out.println("Enter Number : ");
         iValue = sobj.nextInt();
 
-        iRet = dobj.CountOddDigits(iValue);    
+        iRet = dobj.CountOddDigits(iValue);
 
         System.out.println("Number of Odd Digits are : " + iRet);
     }
 }
 
- /*
-    ==============================================================================
-     Function    : CountOddDigits
-
-     Description : Counts the number of odd digits present in the specified
-                   number.
-
-     Parameters  : iNo - Number whose digits are to be examined.
-
-     Returns     : Number of odd digits present in the specified number.
-
-     Complexity  : Time  : O(d)
-                   Space : O(1)
-
-     Note        : d represents the number of digits in the given number.
-    ==============================================================================
-    */
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter Number :
+//      123456
+//
+//  Sample Output :
+//      Number of Odd Digits are : 3
+//
+//////////////////////////////////////////////////////////////////
