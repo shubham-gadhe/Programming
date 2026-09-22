cba4f7558049755a73c5c7fcaa41dd9f273f39f1
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-22 22:22:26 +0530

    Update PP_027.java

--- Java_Programming/Practise_Programs/PP_027.java
+++ Java_Programming/Practise_Programs/PP_027.java
@@ -1,53 +1,47 @@
-/*
-==============================================================================
- Program     : Count Digits of a Number
-
- Description : Reads an integer from the user and counts the number of digits
-               present in the specified number.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_027
+//
+//  Description       : This program accepts a number from the user
+//                      and counts the number of digits present
+//                      in the given number using a while loop.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 10/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 import java.util.Scanner;
 
-/*
-==============================================================================
- Class       : DigitX
-
- Description : Provides utility methods for performing operations on digits.
-==============================================================================
-*/
 class DigitX
 {
-    /*
-    ==============================================================================
-     Function    : CountDigits
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Method Name      : CountDigits()
+    //
+    //  Description      : It is used to count the number of digits
+    //                     present in the given number using modulus
+    //                     and division operations.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 10/07/2026
+    //
+    //////////////////////////////////////////////////////////////////
 
-     Description : Counts the number of digits present in the specified
-                   number.
-
-     Parameters  : iNo - Number whose digits are to be counted.
-
-     Returns     : Number of digits present in the specified number.
-
-     Complexity  : Time  : O(d)
-                   Space : O(1)
-
-     Note        : d represents the number of digits in the given number.
-    ==============================================================================
-    */
     public int CountDigits(int iNo)
     {
         int iDigit = 0;
         int iCount = 0;
 
-         while(iNo != 0)
+        while(iNo != 0)
         {
             iDigit = iNo % 10;
             iCount++;
             iNo = iNo / 10;
-        }  
+        }
 
         return iCount;
     }
@@ -62,24 +56,23 @@ class PP_027
 
         int iValue = 0;
         int iRet = 0;
-        
+
         System.out.println("Enter Number : ");
         iValue = sobj.nextInt();
 
-        iRet = dobj.CountDigits(iValue);    
+        iRet = dobj.CountDigits(iValue);
 
         System.out.println("Number of Digits are : " + iRet);
     }
 }
 
-/*
-==============================================================================
- Sample Input
-------------------------------------------------------------------------------
- 751
-
- Sample Output
-------------------------------------------------------------------------------
- Number of Digits are : 3
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter Number :
+//      12345
+//
+//  Sample Output :
+//      Number of Digits are : 5
+//
+//////////////////////////////////////////////////////////////////
