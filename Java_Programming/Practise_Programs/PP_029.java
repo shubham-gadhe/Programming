0496e5f63f21532718d8fcbc841150919e30d16d
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-22 22:23:49 +0530

    Update PP_029.java

--- Java_Programming/Practise_Programs/PP_029.java
+++ Java_Programming/Practise_Programs/PP_029.java
@@ -1,56 +1,53 @@
-/*
-==============================================================================
- Program     : Count Occurrences of Digit 7
-
- Description : Reads an integer from the user and counts the number of
-               occurrences of the digit 7 in the specified number.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_029
+//
+//  Description       : This program accepts a number from the user
+//                      and counts the occurrence of digit 7 in
+//                      the given number.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 10/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 import java.util.Scanner;
 
-/*
-==============================================================================
- Class       : DigitX
-
- Description : Provides utility methods for performing operations on digits.
-==============================================================================
-*/
 class DigitX
 {
-    /*
-    ==============================================================================
-     Function    : CountDigits
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Method Name      : CountDigits()
+    //
+    //  Description      : It is used to count the occurrence of
+    //                     digit 7 in the given number by extracting
+    //                     each digit using modulus and division.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 10/07/2026
+    //
+    //////////////////////////////////////////////////////////////////
 
-     Description : Counts the number of occurrences of the digit 7 in the
-                   specified number.
-
-     Parameters  : iNo - Number whose digits are to be examined.
-
-     Returns     : Number of occurrences of the digit 7.
-
-     Complexity  : Time  : O(d)
-                   Space : O(1)
-
-     Note        : d represents the number of digits in the given number.
-    ==============================================================================
-    */
     public int CountDigits(int iNo)
     {
         int iDigit = 0;
         int iCount = 0;
 
-         while(iNo != 0)
+        while(iNo != 0)
         {
             iDigit = iNo % 10;
+
             if(iNo == 7)
             {
                 iCount++;
-            }  
-            iNo = iNo / 10;      
-        }  
+            }
+
+            iNo = iNo / 10;
+        }
+
         return iCount;
     }
 }
@@ -64,24 +61,23 @@ class PP_029
 
         int iValue = 0;
         int iRet = 0;
-        
+
         System.out.println("Enter Number : ");
         iValue = sobj.nextInt();
 
-        iRet = dobj.CountDigits(iValue);    
+        iRet = dobj.CountDigits(iValue);
 
         System.out.println("Number of Digits are : " + iRet);
     }
 }
 
-/*
-==============================================================================
- Sample Input
-------------------------------------------------------------------------------
- 78757
-
- Sample Output
-------------------------------------------------------------------------------
- Number of Digits are : 3
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter Number :
+//      127737
+//
+//  Sample Output :
+//      Number of Digits are : 1
+//
+//////////////////////////////////////////////////////////////////
