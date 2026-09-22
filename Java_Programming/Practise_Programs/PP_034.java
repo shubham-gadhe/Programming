e5966a12f87683c605d2e385e637751b3be0a1d5
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-22 22:27:30 +0530

    Update PP_034.java

--- Java_Programming/Practise_Programs/PP_034.java
+++ Java_Programming/Practise_Programs/PP_034.java
@@ -1,51 +1,46 @@
-/*
-==============================================================================
- Program     : Calculate Sum of Digits
-
- Description : Reads an integer from the user and calculates the sum of all
-               digits present in the specified number.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_034
+//
+//  Description       : This program accepts a number from the user
+//                      and calculates the sum of all digits present
+//                      in the given number.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 10/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 import java.util.Scanner;
 
-/*
-==============================================================================
- Class       : DigitX
-
- Description : Provides utility methods for performing operations on digits.
-==============================================================================
-*/
 class DigitX
 {
-    /*
-    ==============================================================================
-     Function    : SumDigits
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Method Name      : SumDigits()
+    //
+    //  Description      : It is used to calculate the sum of all
+    //                     digits present in the given number by
+    //                     extracting each digit and adding it
+    //                     directly to the sum.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 10/07/2026
+    //
+    //////////////////////////////////////////////////////////////////
 
-     Description : Calculates and returns the sum of all digits present in the
-                   specified number.
-
-     Parameters  : iNo - Number whose digits are to be processed.
-
-     Returns     : Sum of all digits present in the specified number.
-
-     Complexity  : Time  : O(d)
-                   Space : O(1)
-
-     Note        : d represents the number of digits in the given number.
-    ==============================================================================
-    */
     public int SumDigits(int iNo)
     {
         int iSum = 0;
 
-         while(iNo != 0)
+        while(iNo != 0)
         {
-            iSum = iSum + (iNo % 10);             
-            iNo = iNo / 10;      
-        }  
+            iSum = iSum + (iNo % 10);
+            iNo = iNo / 10;
+        }
 
         return iSum;
     }
@@ -60,24 +55,23 @@ class PP_034
 
         int iValue = 0;
         int iRet = 0;
-        
+
         System.out.println("Enter Number : ");
         iValue = sobj.nextInt();
 
-        iRet = dobj.SumDigits(iValue);    
+        iRet = dobj.SumDigits(iValue);
 
         System.out.println("Summation of Digits: " + iRet);
     }
 }
 
-/*
-==============================================================================
- Sample Input
-------------------------------------------------------------------------------
- 751
-
- Sample Output
-------------------------------------------------------------------------------
- Summation of Digits : 13
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter Number :
+//      12345
+//
+//  Sample Output :
+//      Summation of Digits: 15
+//
+//////////////////////////////////////////////////////////////////
