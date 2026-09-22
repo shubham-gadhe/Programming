ff5819ca21afa12045025732d2071023850942aa
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-22 22:26:08 +0530

    Update PP_032.java

--- Java_Programming/Practise_Programs/PP_032.java
+++ Java_Programming/Practise_Programs/PP_032.java
@@ -1,60 +1,57 @@
-/*
-==============================================================================
- Program     : Count Even and Odd Digits in a Number
-
- Description : Reads an integer from the user and counts the number of even
-               and odd digits present in the specified number.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_032
+//
+//  Description       : This program accepts a number from the user
+//                      and counts the number of even and odd digits
+//                      present in the given number.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 10/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 import java.util.Scanner;
 
-/*
-==============================================================================
- Class       : DigitX
-
- Description : Provides utility methods for performing operations on digits.
-==============================================================================
-*/
 class DigitX
 {
-    /*
-    ==============================================================================
-     Function    : CountEvenOddDigits
+    //////////////////////////////////////////////////////////////////
+    //
+    //  Method Name      : CountEvenOddDigits()
+    //
+    //  Description      : It is used to count the number of even
+    //                     and odd digits present in the given
+    //                     number by extracting each digit and
+    //                     checking whether it is even or odd.
+    //
+    //  Author           : Shubham Somanath Gadhe
+    //  Date             : 10/07/2026
+    //
+    //////////////////////////////////////////////////////////////////
 
-     Description : Counts the number of even and odd digits present in the
-                   specified number and displays the result.
-
-     Parameters  : iNo - Number whose digits are to be examined.
-
-     Returns     : None
-
-     Complexity  : Time  : O(d)
-                   Space : O(1)
-
-     Note        : d represents the number of digits in the given number.
-    ==============================================================================
-    */
     public void CountEvenOddDigits(int iNo)
     {
         int iDigit = 0;
         int iCountEven = 0, iCountOdd = 0;
 
-         while(iNo != 0)
+        while(iNo != 0)
         {
             iDigit = iNo % 10;
+
             if(iDigit % 2 == 0)
             {
-                iCountOdd++;
-            } 
+                iCountEven++;
+            }
             else
-                {
-                    iCountOdd++;
-                } 
-            iNo = iNo / 10;      
-        }  
+            {
+                iCountOdd++;
+            }
+
+            iNo = iNo / 10;
+        }
 
         System.out.println("Number of even digits : " + iCountEven);
         System.out.println("Number of odd digits : " + iCountOdd);
@@ -69,23 +66,22 @@ class PP_032
         DigitX dobj = new DigitX();
 
         int iValue = 0;
-        
+
         System.out.println("Enter Number : ");
         iValue = sobj.nextInt();
 
-        dobj.CountEvenOddDigits(iValue);    
+        dobj.CountEvenOddDigits(iValue);
     }
 }
 
-/*
-==============================================================================
- Sample Input
-------------------------------------------------------------------------------
- 864257
-
- Sample Output
-------------------------------------------------------------------------------
- Number of even digits : 3
- Number of odd digits  : 3
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter Number :
+//      123456
+//
+//  Sample Output :
+//      Number of even digits : 3
+//      Number of odd digits : 3
+//
+//////////////////////////////////////////////////////////////////
