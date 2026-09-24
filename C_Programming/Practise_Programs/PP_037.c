137e5249f17d85dd31f22965f49242198f09a2c8
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-25 04:45:00 +0530

    Update PP_037.c

--- C_Programming/Practise_Programs/PP_037.c
+++ C_Programming/Practise_Programs/PP_037.c
@@ -1,43 +1,44 @@
-/*
-==============================================================================
- Program     : Display Numbers from 1 to N-1
-
- Description : Reads an integer from the user and displays all numbers from
-               1 up to (but not including) the specified value.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_037
+//
+//  Description       : This program accepts a number from the user
+//                      and displays numbers starting from 1 up to
+//                      one less than the given number.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 08/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
-/*
-==============================================================================
- Function    : Display
+//////////////////////////////////////////////////////////////////
+//
+//  Function Name     : Display()
+//
+//  Description       : It is used to display numbers starting from
+//                      1 up to one less than the given number using
+//                      a for loop.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 08/07/2026
+//
+//////////////////////////////////////////////////////////////////
 
- Description : Displays all numbers from 1 up to (but not including) the
-               specified value.
-
- Parameters  : iNo - Upper limit of the sequence (exclusive).
-
- Returns     : None
-
- Complexity  : Time  : O(n)
-               Space : O(1)
-==============================================================================
-*/
 void Display(int iNo)
 {
     int iCnt = 0;
 
-    // Display numbers from 1 to (iNo - 1).
-    for(iCnt = 1; iCnt < iNo; iCnt = iCnt += 1)
+    for(iCnt = 1; iCnt < iNo; iCnt = iCnt + 1)
     {
         printf("%d\t", iCnt);
     }
-    
-    printf("\n");
 
+    printf("\n");
 }
 
 int main()
@@ -52,14 +53,13 @@ int main()
     return 0;
 }
 
-/*
-==============================================================================
- Sample Input
-------------------------------------------------------------------------------
- 5
-
- Sample Output
-------------------------------------------------------------------------------
- 1    2    3    4
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter number :
+//      5
+//
+//  Sample Output :
+//      1    2    3    4
+//
+//////////////////////////////////////////////////////////////////
