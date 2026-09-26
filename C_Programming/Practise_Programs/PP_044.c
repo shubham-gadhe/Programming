63aea476b72cce135b49c43b56427ffd49c227a3
Shubham Gadhe <shubhamgadhe.dev@gmail.com>
2026-09-26 22:17:33 +0530

    Update PP_044.c

--- C_Programming/Practise_Programs/PP_044.c
+++ C_Programming/Practise_Programs/PP_044.c
@@ -1,42 +1,46 @@
-/*
-==============================================================================
- Program     : Display Alternate Negative Numbers from -N to -1
-
- Description : Reads an integer from the user and displays alternate negative
-               numbers from the negative of the specified value up to -1.
-
- Author      : Shubham Gadhe
-==============================================================================
-*/     
+//////////////////////////////////////////////////////////////////
+//
+//  File Name         : PP_044
+//
+//  Description       : This program accepts a number from the user
+//                      and displays negative numbers starting from
+//                      the negative of the given number with an
+//                      increment of 2 up to -1.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 08/07/2026
+//
+//  Time Complexity   : O(n)
+//  Space Complexity  : O(1)
+//
+//////////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
-/*
-==============================================================================
- Function    : Display
+//////////////////////////////////////////////////////////////////
+//
+//  Function Name     : Display()
+//
+//  Description       : It is used to display negative numbers
+//                      starting from the negative of the given
+//                      number with an increment of 2 using
+//                      a for loop.
+//
+//  Author            : Shubham Somanath Gadhe
+//  Date              : 08/07/2026
+//
+//////////////////////////////////////////////////////////////////
 
- Description : Displays alternate negative numbers from -N to -1.
-
- Parameters  : iNo - Absolute value used to generate the sequence.
-
- Returns     : None
-
- Complexity  : Time  : O(n)
-               Space : O(1)
-==============================================================================
-*/
 void Display(int iNo)
 {
     int iCnt = 0;
 
-    // Display alternate negative numbers from -N to -1.
     for(iCnt = -iNo; iCnt < 0; iCnt = iCnt + 2)
     {
         printf("%d\t", iCnt);
     }
-    
-    printf("\n");
 
+    printf("\n");
 }
 
 int main()
@@ -51,14 +55,13 @@ int main()
     return 0;
 }
 
-/*
-==============================================================================
- Sample Input
-------------------------------------------------------------------------------
- 5
-
- Sample Output
-------------------------------------------------------------------------------
- -5    -3    -1
-==============================================================================
-*/
+//////////////////////////////////////////////////////////////////
+//
+//  Sample Input  :
+//      Enter number :
+//      10
+//
+//  Sample Output :
+//      -10    -8    -6    -4    -2
+//
+//////////////////////////////////////////////////////////////////
